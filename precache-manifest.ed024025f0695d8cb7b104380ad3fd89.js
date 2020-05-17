f6ac3651b418726fdd586e870e0dad62c8ab5dea
Aaron Liu <aaron.liu.bc@gmail.com>
2020-05-17 11:24:06 -0700

    Updates

---
+++ precache-manifest.ed024025f0695d8cb7b104380ad3fd89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65930771302fa58666afc5c3dcc0cf2e",
+    "url": "/foundrymissions-game-react/index.html"
+  },
+  {
+    "revision": "3f2d6c90f02c69608334",
+    "url": "/foundrymissions-game-react/static/css/main.447adcec.chunk.css"
+  },
+  {
+    "revision": "5dafdffafcf27e46cc19",
+    "url": "/foundrymissions-game-react/static/js/2.6f857cfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/foundrymissions-game-react/static/js/2.6f857cfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f2d6c90f02c69608334",
+    "url": "/foundrymissions-game-react/static/js/main.20dd37ca.chunk.js"
+  },
+  {
+    "revision": "71de24ea5125cff1c022",
+    "url": "/foundrymissions-game-react/static/js/runtime-main.b75ffdfa.js"
+  },
+  {
+    "revision": "26ef8bb418031b9bb4f44e1aeea71186",
+    "url": "/foundrymissions-game-react/static/media/loadinggif.26ef8bb4.gif"
+  }
+]);
